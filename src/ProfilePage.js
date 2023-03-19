f697655f67a621b86a0fec49ac3a1fa4f0b569ba
Pradeepraj-james <pradeepraj.james@gmail.com>
2023-03-19 22:39:57 +0530

    add match params

--- src/ProfilePage.js
+++ src/ProfilePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ProfilePage = ({ match }) => {
+const ProfilePage = ({match}) => {
     const [user, setUser] = useState(null);
 
     // Sending request to Server
